593693611070b96390e35e9cbd9c3fdd6c9f0c8e
sharad461 <sharad.duwal@gmail.com>
2018-12-15 13:45:45 +0545

    flask template

--- sent-al/app.py
+++ sent-al/app.py
@@ -2,11 +2,11 @@ import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
 port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
 def index():
     return render_template("home.html")
 
-app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == "__main__":
+	app.run(port=port, debug=True)
