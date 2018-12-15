9470de6207aea6c7d4674f312432cb8e655e8dba
sharad461 <sharad.duwal@gmail.com>
2018-12-15 13:05:51 +0545

    heroku ready

--- sent-al/app.py
+++ sent-al/app.py
@@ -7,6 +7,6 @@ port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
 app.run(host='0.0.0.0', port=port, debug=True)
