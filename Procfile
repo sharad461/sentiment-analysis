88a937e3672dace10c8ac0a83068fccb9d35c6e1
sharad461 <sharad.duwal@gmail.com>
2018-12-15 12:45:48 +0545

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: gunicorn -w 4 -b 0.0.0.0:33507 -k gevent app:app
--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
-@app.route("/")
-def index():
-	return render_template('home.html')
-
-if __name__ == "__main__":
-	app.run(debug = True, port=33507)
+@app.route('/')
+def home():
+    return render_template('home.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
