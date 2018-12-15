ab8cb6daa60207cd0ab6368fdfb568b590d7dd68
sharad461 <sharad.duwal@gmail.com>
2018-12-15 12:57:03 +0545

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -b 0.0.0.0:33507 -k gevent app:app
+web: python app.py $PORT
--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask, render_template
 
-app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+app = Flask('MyHerokuApp')
+
+# Read the mailgun secret key from environment variables
+mailgun_secret_key_value = os.environ.get('MAILGUN_SECRET_KEY', None)
+
+# This is needed for Heroku configuration, as in Heroku our
+# app will probably not run on port 5000, as Heroku will automatically
+# assign a port for our application
+port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-    
-if __name__ == '__main__':
-    app.run(debug=True)
+def index():
+
+    # We will just display our mailgun secret key, nothing more.
+    return render_template("index.html", value=mailgun_secret_key_value)
+
+# Route that will get the config value based on a provided key, so in
+# this way we can interogate our configuration.
+@app.route('/<config_key>')
+def key(config_key=None):
+    config_value=None
+
+    if (config_key):
+        # Advice: You should make a convention to define config key upper cased.
+        # otherwise you will have consistency issues when reading your keys.
+        # If you know they are always upper cased, you just need to uppercase
+        # what the config_key argument.
+        config_key = config_key.upper()
+        config_value = os.environ.get(config_key, None)
+
+    if key and config_value:
+        app.logger.info("Value of {} is {}.".format(config_key, config_value))
+
+    # We will just display our mailgun secret key, nothing more.
+    return render_template("keys.html", key=config_key, value=config_value)
+
+app.run(host='0.0.0.0', port=port, debug=True)
